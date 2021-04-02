29086aef131063c8030330ca0a6265377f3a41b7
romeo adjam <sodangbe@users.noreply.github.com>
2021-04-01 20:46:06 -0500

    Added last query

--- queries.sql
+++ queries.sql
@@ -5,3 +5,7 @@ SELECT * FROM techjobs.employer where location = "Saint Louis" ;
 ## Part 3: Test it with SQL
 DROP TABLE `techjobs`."job";
 ## Part 4: Test it with SQL
+SELECT techjobs.skill.name , techjobs.skill.description
+FROM techjobs.skill
+INNER JOIN techjobs.job ON techjobs.skill.description = techjobs.job.skills
+ORDER BY techjobs.skill.name  DESC ;
